0044759234fb52640c59ff540a1cbee8c51f50a8
Gleb Danylov <gleb_dan@hotmail.com>
2021-05-23 00:56:31 +0300

    add autorization

---
+++ index.js
@@ -0,0 +1,9 @@
+const autorizationField = document.getElementById("a");
+const title = document.getElementById("t");
+function enter() {
+    autorizationField.style.display = "none";
+}
+
+function reg() {
+    title.innerText = "Регистрируйтесь!";
+}
